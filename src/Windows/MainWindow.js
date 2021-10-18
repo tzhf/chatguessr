86827d9a18a5537e4d9532d927ac61127a963543
ReAnna <i-am-anna@outlook.com>
2021-10-18 18:07:18 +0200

    bundle the renderer code that runs fully in the browser

--- src/Windows/MainWindow.js
+++ src/Windows/MainWindow.js
@@ -1,11 +1,10 @@
 const path = require("path");
+const fs = require('fs');
 const { BrowserWindow, shell } = require("electron");
 /** @type {string} */
 // @ts-ignore
 const styles = require('bundle-text:../../assets/styles.css');
-/** @type {string} */
-// @ts-ignore
-const js = require('bundle-text:../renderer.js');
+const js = fs.readFileSync(path.join(__dirname, '../cg-renderer/renderer.js'), 'utf8');
 
 function mainWindow() {
 	let win = new BrowserWindow({

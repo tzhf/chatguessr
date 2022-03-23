a151625e03c22d6aa89d19290c346ef9714203d3
unknown <theo.zahaf@gmail.com>
2022-03-23 12:18:39 +0100

    new autoUpdater + .deb fix

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # ChatGuessr
 
 > Play GeoGuessr on Stream with Twitch Chat.
-
-# Deploy
-
-1. Run windows powerShell from Administrator
-2. Execute [Environment]::SetEnvironmentVariable("GH_TOKEN","<git_hub_token>","User")
-3. \$ npm run deploy
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-'use strict';
+"use strict";
 
-require('./errorReporting');
+require("./errorReporting");
 
-const path = require('path');
-const { app, ipcMain, globalShortcut, protocol } = require("electron");
-const { initRenderer } = require('electron-store');
-const { autoUpdater } = require("electron-updater");
+require("update-electron-app")();
+
+const path = require("path");
+const { app, globalShortcut, protocol } = require("electron");
+const { initRenderer } = require("electron-store");
 const GameHandler = require("./GameHandler").default;
-const flags = require('./utils/flags');
-const Database = require('./utils/Database');
+const flags = require("./utils/flags");
+const Database = require("./utils/Database");
 
 /** @type {import('electron').BrowserWindow} */
 let mainWindow;
 
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
 	app.quit();
 }
 
@@ -23,15 +24,15 @@ app.on("window-all-closed", () => {
 	}
 });
 
-const dbPath = path.join(app.getPath('userData'), 'scores.db');
+const dbPath = path.join(app.getPath("userData"), "scores.db");
 const db = new Database(dbPath);
 
 function serveAssets() {
-	const assetDir = path.join(__dirname, '../../assets');
-	protocol.interceptFileProtocol('asset', (request, callback) => {
+	const assetDir = path.join(__dirname, "../../assets");
+	protocol.interceptFileProtocol("asset", (request, callback) => {
 		const assetFile = path.join(assetDir, new URL(request.url).pathname);
 		if (!assetFile.startsWith(assetDir)) {
-			callback({ statusCode: 404, data: 'Not Found' });
+			callback({ statusCode: 404, data: "Not Found" });
 		} else {
 			callback({ path: assetFile });
 		}
@@ -41,8 +42,8 @@ function serveAssets() {
 async function serveFlags() {
 	await flags.load();
 
-	protocol.interceptFileProtocol('flag', async (request, callback) => {
-		const name = request.url.replace(/^flag:/, '');
+	protocol.interceptFileProtocol("flag", async (request, callback) => {
+		const name = request.url.replace(/^flag:/, "");
 		try {
 			callback(await flags.findFlagFile(name));
 		} catch (err) {
@@ -55,9 +56,6 @@ function initWindow() {
 	mainWindow = require("./Windows/MainWindow");
 	mainWindow.once("ready-to-show", () => {
 		mainWindow.maximize();
-		setTimeout(() => {
-			autoUpdater.checkForUpdatesAndNotify();
-		}, 2000);
 	});
 
 	const gameHandler = new GameHandler(db, mainWindow);
@@ -81,33 +79,5 @@ async function init() {
 
 	initWindow();
 }
-
-// Auto Updater
-let updateWindow;
-autoUpdater.on("update-available", () => {
-	updateWindow = require("./Windows/update/UpdateWindow");
-	updateWindow.setParentWindow(mainWindow);
-	// updateWindow.webContents.send("update_available");
-});
-
-autoUpdater.on("download-progress", () => {
-	updateWindow.webContents.send("download_progress");
-});
-
-autoUpdater.on("update-downloaded", () => {
-	updateWindow.webContents.send("update_downloaded");
-});
-
-autoUpdater.on("error", (e, err) => {
-	updateWindow.webContents.send("update_error", err);
-});
-
-ipcMain.on("restart_app", () => {
-	autoUpdater.quitAndInstall();
-});
-
-ipcMain.on("close_update_window", () => {
-	updateWindow.hide();
-});
 
 init();

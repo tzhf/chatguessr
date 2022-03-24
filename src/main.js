4493e6de6c0f4b85920d67d77919d0a05e2eee73
unknown <theo.zahaf@gmail.com>
2022-03-24 06:53:55 +0100

    window now showing on mac fixed maybe ?

--- src/main.js
+++ src/main.js
@@ -5,24 +5,18 @@ require("./errorReporting");
 if (process.platform == "win32") require("update-electron-app")();
 
 const path = require("path");
-const { app, globalShortcut, protocol } = require("electron");
+const { app, BrowserWindow, globalShortcut, protocol } = require("electron");
 const { initRenderer } = require("electron-store");
 const GameHandler = require("./GameHandler").default;
 const flags = require("./utils/flags");
 const Database = require("./utils/Database");
 
-/** @type {import('electron').BrowserWindow} */
-let mainWindow;
+// /** @type {import('electron').BrowserWindow} */
+// let mainWindow;
 
 if (require("electron-squirrel-startup")) {
 	app.quit();
 }
-
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
-});
 
 const dbPath = path.join(app.getPath("userData"), "scores.db");
 const db = new Database(dbPath);
@@ -52,8 +46,10 @@ async function serveFlags() {
 	});
 }
 
-function initWindow() {
-	mainWindow = require("./Windows/MainWindow");
+function createWindow() {
+	/** @type {import('electron').BrowserWindow} */
+	const mainWindow = require("./Windows/MainWindow");
+
 	mainWindow.once("ready-to-show", () => {
 		mainWindow.maximize();
 	});
@@ -72,12 +68,30 @@ function initWindow() {
 
 async function init() {
 	initRenderer();
-	await app.whenReady();
 
-	serveAssets();
-	await serveFlags();
+	app.whenReady().then(async () => {
+		serveAssets();
+		await serveFlags();
 
-	initWindow();
+		createWindow();
+
+		app.on("activate", () => {
+			// On OS X it's common to re-create a window in the app when the
+			// dock icon is clicked and there are no other windows open.
+			if (BrowserWindow.getAllWindows().length === 0) {
+				createWindow();
+			}
+		});
+	});
 }
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
 
 init();

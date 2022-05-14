c1cd86df4d49437921c8a90b1e8dd229e4eb3c1a
emily nagy <72168929+itisem@users.noreply.github.com>
2022-05-14 11:34:28 +0200

    Update src/modules.d.ts
    Co-authored-by: ReAnnannanna <100017889+ReAnnannanna@users.noreply.github.com>

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,5 +1,6 @@
-declare function coordinate_to_country(lat: Number, lng: Number, isoA2: Boolean?){
-    return [String];
+declare module 'coordinate_to_country' {
+    function coordinate_to_country(lat: number, lng: number, isoA2?: boolean): string[];
+    export = coordinate_to_country;
 }
 
 declare module 'electron-squirrel-startup' {

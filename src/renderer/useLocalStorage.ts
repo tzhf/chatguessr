415b3110c362f90565ba2ebe124c1911bff00308
tzhf <theo.zahaf@gmail.com>
2025-03-14 19:36:16 +0100

    update deps

--- src/renderer/useLocalStorage.ts
+++ src/renderer/useLocalStorage.ts
@@ -1,18 +1,14 @@
 export function getLocalStorage<T>(key: string, initialValue: T): T {
   const storedVal = window.localStorage.getItem(key)
-  if (storedVal) return isJsonString(storedVal) ? JSON.parse(storedVal) : storedVal
-  return initialValue
+  if (!storedVal) return initialValue
+
+  try {
+    return JSON.parse(storedVal)
+  } catch {
+    return initialValue
+  }
 }
 
 export function setLocalStorage<T>(key: string, value: T) {
   window.localStorage.setItem(key, JSON.stringify(value))
-}
-
-function isJsonString(str: string) {
-  try {
-    JSON.parse(str)
-  } catch (e) {
-    return false
-  }
-  return true
 }
